dc8f926aa5f2a8208b5f1c709a207d57c106524e
mbpassos <marilia.bpassos@gmail.com>
2021-05-20 09:31:50 +0100

    add jobs component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>Homepage</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure tenetur recusandae unde iusto labore natus libero, delectus ullam. Fuga natus sint et dignissimos quia eaque? Dignissimos itaque ullam temporibus voluptatibus!100</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure tenetur recusandae unde iusto labore natus libero, delectus ullam. Fuga natus sint et dignissimos quia eaque? Dignissimos itaque ullam temporibus voluptatibus!100</p>
   </div>
 </template>
 
